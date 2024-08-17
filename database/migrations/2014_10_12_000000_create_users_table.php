e1f55684d0c84c60317773417a4bc4fb710210fa
Ashik19aug <ashik19aug@gmail.com>
2024-08-17 16:14:00 +0600

    Auth basic implementation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('gender', ['male', 'female', 'other'])->default('other');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
